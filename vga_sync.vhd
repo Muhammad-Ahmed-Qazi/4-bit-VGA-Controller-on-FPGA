cba670c2b5c272ec29bc9288a6c5cec6af142872
Muhammad-Ahmed-Qazi <qmuhammadahmed@gmail.com>
2025-03-25 12:06:46 +0500

    VGA Sync module for 640x480 @ 60Hz display complete.

--- vga_sync.vhd
+++ vga_sync.vhd
@@ -7,8 +7,8 @@ use ieee.numeric_std.all;
 entity vga_sync is
     port (
         clk, reset: in std_logic;
-        hsync, video_on, p_tick: out std_logic;
-        pixel_x: out std_logic_vector (9 downto 0)
+        hsync, vsync, video_on, p_tick: out std_logic;
+        pixel_x, pixel_y: out std_logic_vector (9 downto 0)
     );
 end vga_sync;
 
@@ -20,16 +20,22 @@ architecture arch of vga_sync is
     constant HR: integer := 96;   -- H. retrace
     constant HB: integer := 48;   -- H. back porch
 
+    constant VD: integer := 480;  -- V. display area
+    constant VF: integer := 10;   -- V. front porch
+    constant VR: integer := 2;    -- V. retrace
+    constant VB: integer := 33;   -- V. back porch
+
     -- Mod-2 counter
     signal mod2_reg, mod2_next: std_logic;
     -- Sync counters
     signal h_count_reg, h_count_next: std_logic_vector (9 downto 0);
+    signal v_count_reg, v_count_next: std_logic_vector (9 downto 0);
     -- Output buffer
-    signal h_sync_reg: std_logic;
-    signal h_sync_next: std_logic;
+    signal h_sync_reg, v_sync_reg: std_logic;
+    signal h_sync_next, v_sync_next: std_logic;
 
     -- Status signal
-    signal h_end, pixel_tick: std_logic;
+    signal h_end, v_end, pixel_tick: std_logic;
 
 begin
 
@@ -38,11 +44,15 @@ begin
     begin
         if reset = '1' then
             mod2_reg <= '0';
+            v_count_reg <= (others => '0');
             h_count_reg <= (others => '0');
+            v_sync_reg <= '0';
             h_sync_reg <= '0';
         elsif rising_edge(clk) then
             mod2_reg <= mod2_next;
+            v_count_reg <= v_count_next;
             h_count_reg <= h_count_next;
+            v_sync_reg <= v_sync_next;
             h_sync_reg <= h_sync_next;
         end if;
     end process;
@@ -54,8 +64,8 @@ begin
 
     -- Status
     --h_end <= '1' when (h_count_reg = (HD+HF+HR+HB-1)) else '0';
-	 h_end <= '1' when (unsigned(h_count_reg) = (HD+HF+HR+HB-1)) else '0';
-
+	h_end <= '1' when (unsigned(h_count_reg) = (HD+HF+HR+HB-1)) else '0';
+    v_end <= '1' when (unsigned(v_count_reg) = (VD+VF+VR+VB-1)) else '0';
 	 
     -- Mod-800 Horizontal Synchronisation
     process (h_count_reg, h_end, pixel_tick)
@@ -71,15 +81,33 @@ begin
         end if;
     end process;
 
+    -- Mod-525 Vertical Synchronisation
+    process (v_count_reg, v_end, h_end, pixel_tick)
+    begin
+        if pixel_tick = '1' and h_end = '1' then
+            if (v_end = '1') then
+                v_count_next <= (others => '0');
+            else
+                v_count_next <= std_logic_vector(unsigned(v_count_reg) + 1);
+            end if;
+        else
+            v_count_next <= v_count_reg;
+        end if;
+    end process;
+
     -- Horizontal sync, buffered to avoid glitch
-    h_sync_next <= '1' when (unsigned(h_count_reg) >= HD+HF) and (unsigned(h_count_reg) < HD+HF+HR) else '0';
+    h_sync_next <= '0' when (unsigned(h_count_reg) >= HD+HF) and (unsigned(h_count_reg) < HD+HF+HR) else '1';
+    -- Vertical sync, buffered to avoid glitch
+    v_sync_next <= '0' when (unsigned(v_count_reg) >= VD+VF) and (unsigned(v_count_reg) < VD+VF+VR) else '1';
     
     -- Video ON/OFF
-    video_on <= '1' when (unsigned(h_count_reg) < HD) else '0';
+    video_on <= '1' when (unsigned(h_count_reg) < HD) and (unsigned(v_count_reg) < VD) else '0';
 
     -- Output
     hsync <= h_sync_reg;
+    vsync <= v_sync_reg;
     pixel_x <= std_logic_vector(h_count_reg);
+    pixel_y <= std_logic_vector(v_count_reg);
     p_tick <= pixel_tick;
 
 end arch ; -- arch
--- vga_sync_tb.vhd
+++ vga_sync_tb.vhd
@@ -8,8 +8,9 @@ END vga_sync_tb;
 ARCHITECTURE testbench OF vga_sync_tb IS
     -- Declare signals to connect to the UUT
     SIGNAL clk, reset: STD_LOGIC := '0';
-    SIGNAL hsync, video_on: STD_LOGIC;
-    SIGNAL pixel_x: STD_LOGIC_VECTOR(9 DOWNTO 0);
+    SIGNAL hsync, vsync, video_on: STD_LOGIC;
+    SIGNAL p_tick: STD_LOGIC;
+    SIGNAL pixel_x, pixel_y: STD_LOGIC_VECTOR(9 DOWNTO 0);
     
     -- Clock period
     CONSTANT clk_period : TIME := 40 ns;  -- 25 MHz clock
@@ -21,18 +22,21 @@ BEGIN
         clk => clk,
         reset => reset,
         hsync => hsync,
+        vsync => vsync,
         video_on => video_on,
-        pixel_x => pixel_x
+        p_tick => p_tick,
+        pixel_x => pixel_x,
+        pixel_y => pixel_y
     );
 
     -- Clock process
     clk_process : PROCESS
     BEGIN
-        WHILE NOW < 10 ms LOOP  -- Run simulation for 10 ms
+        WHILE NOW < 20 ms LOOP  -- Run simulation for 20 ms
             clk <= '0';
-            WAIT FOR clk_period / 2;
+            WAIT FOR clk_period / 4;
             clk <= '1';
-            WAIT FOR clk_period / 2;
+            WAIT FOR clk_period / 4;
         END LOOP;
         WAIT;
     END PROCESS;
@@ -46,7 +50,7 @@ BEGIN
         reset <= '0';
         
         -- Run for some time to observe the sync signals
-        WAIT FOR 10 ms;
+        WAIT FOR 20 ms;
         
         -- Stop the simulation
         WAIT;
